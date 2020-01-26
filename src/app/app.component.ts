58010a085ceb9a117d6db3b73268581f005341d9
Sushmitha <sushmithaspoojary4@gmail.com>
2020-01-26 08:17:04 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,5 @@ export class AppComponent implements OnInit {
   title = "swiggyApp";
 
   ngOnInit() {
-    var firebaseConfig = {
-      apiKey: "AIzaSyA--WC-RbtA4g8XwiaNYbnUoycvgxI-ftE",
-      authDomain: "swiggy-af853.firebaseapp.com",
-      databaseURL: "https://swiggy-af853.firebaseio.com",
-      projectId: "swiggy-af853",
-      storageBucket: "swiggy-af853.appspot.com",
-      messagingSenderId: "1093002151603",
-      appId: "1:1093002151603:web:e2978e3263c01e213892c6"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-  }
+    
 }
